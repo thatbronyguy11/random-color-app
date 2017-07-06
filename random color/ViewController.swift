31c0cfe5247147fb370dc5677dc58e7cbb712547
Jackson Dunham <jackson@Jacksons-MacBook-Pro.local>
2017-07-06 11:32:01 -0700

    Initial Commit

---
+++ random color/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  random color
+//
+//  Created by Jackson Dunham on 7/6/17.
+//  Copyright © 2017 Jackson Dunham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
