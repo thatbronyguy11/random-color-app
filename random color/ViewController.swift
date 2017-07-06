acef2b8ef23b4984484eb48d128a188a5f44440b
Jackson Dunham <theopminer@gmail.com>
2017-07-06 15:05:05 -0700

    working

--- random color/ViewController.swift
+++ random color/ViewController.swift
@@ -13,13 +13,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.randColor()
+        
+    //plan: run \/this\/ in 3 if statments, randomly picking a number between 0 and 2, and have it be randomly generated, and changed, every time the button is pressed.
     }
+    func randColor() {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let rint = arc4random_uniform(256)
+        let r = CGFloat(rint)
+        let gint = arc4random_uniform(256)
+        let g = CGFloat(gint)
+        let bint = arc4random_uniform(256)
+        let b = CGFloat(bint)
+        let bg = UIColor(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: 1.0)
+        self.view.backgroundColor = bg
     }
-
-
+    @IBAction func colorButton(_ sender: UIButton) {
+        randColor()
+    }
 }
 
